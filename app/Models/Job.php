f35b88065529561f4abe077ba182dc902c4327e5
serii burduja <seriiburduja@gmail.com>
2024-11-25 13:02:39 +0200

    feat: create migration to add fields in job_litning with Model filled type

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,5 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     protected $table = 'job_listings';
-    protected $fillable = ['title', 'description'];
+    protected $fillable = [
+        'title',
+        'description',
+        'salary',
+        'tags',
+        'job_type',
+        'remote',
+        'requirements',
+        'benefits',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+        'contract_email',
+        'contract_phone',
+        'company_name',
+        'company_description',
+        'company_logo',
+        'company_website'
+    ];
 }
---
+++ database/migrations/2024_11_25_105402_add_fields_to_job_listings_table.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        // clear table data
+        DB::table('job_listings')->truncate();
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->integer('salary');
+            $table->string('tags')->nullable();
+            $table->enum('job_type', ['full-time', 'part-time', 'contract', 'internship', 'temporary'])->default('full-time');
+            $table->boolean('remote')->default(false);
+            $table->string('requirements')->nullable();
+            $table->string('benefits')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->string('zipcode')->nullable();
+            $table->string('contract_email');
+            $table->string('contract_phone')->nullable();
+            $table->string('company_name');
+            $table->string('company_description')->nullable();
+            $table->string('company_logo')->nullable();
+            $table->string('company_website')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->dropColumn([
+                'salary',
+                'tags',
+                'job_type',
+                'remote',
+                'requirements',
+                'benefits',
+                'address',
+                'city',
+                'state',
+                'zipcode',
+                'contract_email',
+                'contract_phone',
+                'company_name',
+                'company_description',
+                'company_logo',
+                'company_website'
+            ]);
+        });
+    }
+};
