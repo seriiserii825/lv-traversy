ea745a9ab21f1808bcff0b04d1280d8a6552d393
serii burduja <seriiburduja@gmail.com>
2024-11-25 15:58:01 +0200

    feat: job factory and seeder

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -17,7 +17,25 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => 1,
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'salary' => $this->faker->numberBetween(30000, 100000),
+            'tags' => $this->faker->words(3, true),
+            'job_type' => $this->faker->randomElement(['full-time', 'part-time', 'contract', 'internship', 'temporary']),
+            'remote' => $this->faker->boolean,
+            'requirements' => $this->faker->paragraph,
+            'benefits' => $this->faker->paragraph,
+            'address' => $this->faker->streetAddress,
+            'city' => $this->faker->city,
+            'state' => $this->faker->state,
+            'zipcode' => $this->faker->postcode,
+            'contract_email' => $this->faker->safeEmail,
+            'contract_phone' => $this->faker->phoneNumber,
+            'company_name' => $this->faker->company,
+            'company_description' => $this->faker->paragraph,
+            'company_logo' => $this->faker->imageUrl(),
+            'company_website' => $this->faker->url,
         ];
     }
 }
--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Job;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,28 +13,7 @@ class JobSeeder extends Seeder
      */
     public function run(): void
     {
-        $jobs = [
-            [
-                'title' => 'Software Engineer',
-                'description' => 'Develops software solutions by studying information needs, conferring with users, and studying systems flow, data usage, and work processes.',
-            ],
-            [
-                'title' => 'Data Scientist',
-                'description' => 'Data scientists are big data wranglers, gathering and analyzing large sets of structured and unstructured data.',
-            ],
-            [
-                'title' => 'Product Manager',
-                'description' => 'Product managers are responsible for guiding the success of a product and leading the cross-functional team that is responsible for improving it.',
-            ],
-            [
-                'title' => 'DevOps Engineer',
-                'description' => 'DevOps engineers work with developers and the IT staff to oversee the code releases.',
-            ],
-            [
-                'title' => 'Network Engineer',
-                'description' => 'Network engineers are responsible for building and maintaining the day-to-day operation of computer networks that companies and organizations rely on.',
-            ],
-        ];
-        DB::table('job_listings')->insert($jobs);
+        DB::table('job_listings')->truncate();
+        Job::factory()->count(10)->create();
     }
 }
