7ccd9e6c4de440b2cba09224a8509a796e97bb09
serii burduja <seriiburduja@gmail.com>
2025-01-20 12:56:46 +0200

    feat: banner bottom

---
+++ resources/views/components/banner-bottom.blade.php
@@ -0,0 +1,13 @@
+<section class="container mx-auto my-6">
+    <div class="flex flex-col items-center justify-between p-4 text-white bg-blue-800 rounded md:flex-row gap-4">
+        <div>
+            <h2 class="text-xl font-semibold">Looking to hire?</h2>
+            <p class="mt-2 text-lg text-gray-200">
+                Post your job listing now and find the perfect
+                candidate.
+            </p>
+        </div>
+        <x-button-link url="/jobs/create" icon="fa-edit" hover_class="hover:bg-pink-500">Create Job</x-button-link>
+    </div>
+</section>
+
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,4 +2,5 @@
     <x-slot name="title">Worktopia Index</x-slot>
     <h1>Home index</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+    <x-banner-bottom/>
 </x-layout>
