f5eb5a0dac04ca883b89d938fc39f7ade23d4ae7
serii burduja <seriiburduja@gmail.com>
2025-01-20 11:53:32 +0200

    feat: created hero component

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,19 @@
+<section class="relative flex items-center bg-center bg-no-repeat bg-cover hero h-80">
+    <div class="overlay"></div>
+    <div class="container z-10 mx-auto text-center">
+        <h2 class="mb-8 text-4xl font-bold text-white md:text-5xl">
+            Find Your Dream Job
+        </h2>
+        <form class="block mx-5 space-y-2 md:mx-auto md:space-x-2">
+            <input type="text" name="keywords" placeholder="Keywords"
+                class="w-full px-4 py-3 md:w-72 focus:outline-none" />
+            <input type="text" name="location" placeholder="Location"
+                class="w-full px-4 py-3 md:w-72 focus:outline-none" />
+            <button
+                class="w-full px-4 py-3 text-white bg-blue-700 md:w-auto hover:bg-blue-600 focus:outline-none">
+                <i class="mr-1 fa fa-search"></i> Search
+            </button>
+        </form>
+    </div>
+</section>
+
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,11 +6,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
     @vite('resources/css/app.css')
     <title>{{ $title ?? 'Worktopia' }}</title>
 </head>
 <body class="bg-gray-100">
     <x-header />
+    @if (request()->is('/'))
+        <x-hero />
+    @endif
     <main class="container p-4 m-4 mx-auto">
         {{ $slot }}
     </main>
