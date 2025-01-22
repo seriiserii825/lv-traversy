999b8a005b2680640bd98e8f7cfac97c5a467b90
serii burduja <seriiburduja@gmail.com>
2025-01-22 10:10:22 +0200

    feat: jobs show send email apply now

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -59,7 +59,7 @@
                         Put "Job Application" as the subject of your email
                         and attach your resume.
                     </p>
-                    <a href="mailto:manager@company.com"
+                    <a href="mailto:{{ $job->contact_email }}" target="_blank"
                         class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
                         Apply Now
                     </a>
