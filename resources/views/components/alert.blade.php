66facff32cffe604ef403a002650d0656e1fd65b
serii burduja <seriiburduja@gmail.com>
2025-01-27 08:03:06 +0200

    feat: alert with alpine js

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,11 @@
 @props(['type' => 'success', 'message' => ''])
 
 @if (session()->has($type))
-    <div class="p-4 mb-4 text-white rouded-2 {{ $type === 'success' ? 'bg-green-500' : 'bg-red-500' }}">
+    <div
+        x-data="{ show: true }"
+        x-show="show"
+        x-init="setTimeout(() => show = false, 4000)"
+        class="p-4 mb-4 text-white rouded-2 {{ $type === 'success' ? 'bg-green-500' : 'bg-red-500' }}">
         {{ $message }}
     </div>
 @endif
