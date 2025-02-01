052ccf2fdc11749b0e4ddcc308eb4f3281504af5
serii burduja <seriiburduja@gmail.com>
2025-02-01 17:25:37 +0200

    feat: finished register

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -11,9 +13,25 @@ class AuthController extends Controller
     {
         return view('auth.register');
     }
-    public function registerStore() {}
-        public function loginView() {
+    public function registerStore(Request $request) {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|same:password',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return redirect()->route('login.view')->with('success', 'User created successfully');
+    }
+    public function loginView()
+    {
         return view('auth.login');
-        }
+    }
     public function loginStore() {}
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,25 @@
 <x-layout>
-    <h2>register</h2>
+    <div class="min-h-screen flex items-center justify-center  bg-gray-100">
+        <div class="bg-white p-8 rounded-lg shadow-lg  w-full max-w-xl mx-auto ">
+            <h2 class="text-2xl font-bold mb-6 text-center">Register</h2>
+            <form method="POST" action="{{ route('register.store') }}">
+                @csrf
+                <x-input-component label="Full Name" name="name" type="text" required />
+                <x-input-component label="Email" name="email" type="email" required />
+                <x-input-component label="Password" name="password" type="password" required />
+                <x-input-component label="Confirm Password" name="password_confirmation" type="password" required />
+                <div class="mb-6">
+                    <button type="submit"
+                        class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        Register
+                    </button>
+                </div>
+                <div class="text-center">
+                    <a href="{{ route('login.view') }}" class="text-blue-500 hover:underline">Already have an account?
+                        Login</a>
+                </div>
+            </form>
+        </div>
+    </div>
+
 </x-layout>
