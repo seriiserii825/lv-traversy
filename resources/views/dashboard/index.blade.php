8fdc63d640b4f58875199e953614367a86fd41e7
serii burduja <seriiburduja@gmail.com>
2025-02-03 22:50:50 +0200

    feat: finished dashboard styles

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,8 +3,11 @@
         <h2 class="text-xl">My Jobs</h2>
     </div>
     @forelse($jobs as $job)
-        <div class="bg-white rounded-2 py-2 text-center font-bold flex align-items-center px-4 mb-1 shadow-sm border-gray-100">
-            <a href="{{ route('jobs.show', $job) }}" class="text-blue-500 mr-auto">{{ $job->title }}</a>
+        <div class="bg-white rounded-2 py-2 text-center font-bold flex items-center px-4 mb-1 shadow-sm border-gray-100">
+            <a href="{{ route('jobs.show', $job) }}" class="text-gray-700 mr-auto">
+                <p>{{ $job->title }}</p>
+                <p class="text-gray-400 text-left italic">{{ $job->job_type }}</p>
+            </a>
             <span class=" ml-auto text-xs p-2 bg-slate-100 rounded-3 mr-6 text-gray-400">{{ $job->updated_at }}</span>
             <a href="{{ route('jobs.edit', $job) }}?from_dashboard=true"
                 class="px-4 py-1 text-white bg-blue-500 rounded hover:bg-blue-600">Edit</a>
