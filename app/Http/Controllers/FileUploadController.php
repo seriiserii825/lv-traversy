e7b090ca4ffc7bbe59513666a11716ba038368fe
serii burduja <seriiburduja@gmail.com>
2025-02-21 08:44:09 +0200

    feat: upload and show images

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\File;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class FileUploadController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        return view('file.upload');
+        $files = File::all();
+        return view('file.upload', compact('files'));
     }
     public function store(Request $request)
     {
         // $file = Storage::disk('local')->put('/', $request->file('file'));
         // $file = $request->file('file')->store('/', 'local');
-        $file = $request->file('file')->store('/', 'public');
-        dd($file);
+        $file_path = $request->file('file')->store('/', 'public');
+        $file = new File();
+        $file->file_path = $file_path;
+        $file->save();
+        return redirect()->route('file.upload');
     }
 }
--- resources/views/file/upload.blade.php
+++ resources/views/file/upload.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <form action="{{ route('file.store') }}" method="post" enctype="multipart/form-data" class="w-full max-w-sm mx-auto">
+    <form action="{{ route('file.store') }}" method="post" enctype="multipart/form-data" class="w-full max-w-sm mx-auto mb-5">
         @csrf
         <div class="flex items-center border-b border-b-2 border-teal-500 py-2">
             <input type="file" name="file" class="appearance" id="file" required>
@@ -7,4 +7,13 @@
                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Upload</button>
         </div>
     </form>
+    <div class="flex gap-3">
+        @forelse($files as $file)
+            <div>
+                <img src="/storage/{{ $file->file_path }}" alt="" />
+            </div>
+        @empty
+            <p>No files uploaded</p>
+        @endforelse
+    </div>
 </x-layout>
